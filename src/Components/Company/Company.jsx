d1d50ba7b28ec24d63dd5a9c2a3f0b8428b37ff0
PankajNarwade28 <pankajnarwade258@gmail.com>
2024-08-09 22:54:19 +0530

    ss

--- src/Components/Company/Company.jsx
+++ src/Components/Company/Company.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./Company.css";
+import Company1 from "/assets/Company1.png";
+import Company2 from "/assets/Company2.png";
+import Company3 from "/assets/Company3.png";
+import Company4 from "/assets/Company4.png";
+import Company5 from "/assets/Company5.png";
 export const Company = () => {
   return (
     <>
       <div className="company-section">
-        <img src="../src/assets/Company1.png" alt="Company1" />
-        <img src="../src/assets/Company2.png" alt="Company1" />
-        <img src="../src/assets/Company3.png" alt="Company1" />
-        <img src="../src/assets/Company4.png" alt="Company1" />
-        <img src="../src/assets/Company5.png" alt="Company1" />
+        <img src={Company1} alt="Company1" />
+        <img src={Company2} alt="Company1" />
+        <img src={Company3} alt="Company1" />
+        <img src={Company4} alt="Company1" />
+        <img src={Company5} alt="Company1" />
       </div>
     </>
   );
